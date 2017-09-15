7cf413d39dea2f72ca6c4a566c451b699441b464
jonathan.essex <jonathan.essex@misys.com>
2017-09-15 07:53:20 -0400

    prevent elementFactory option from propagating to child element patch operations

--- options.js
+++ options.js
@@ -4,33 +4,33 @@
 
 /** Default options for diff 
  *
- *   | Option                    | Default    | Description
- *   |---------------------------|------------|-------------------------- 
- *   | `elementFactory`          | `false`    | Factory used to create new object instances 
- *   | `mergeInPlace`            | `false`    | Patch will copy properties into existing object rather than creating a new one
- *   | `key`                     | `e=>e[0]`  | Function to extract key from a map entry
- *   | `value`                   | `e=>e[1]`  | Function that extracts value from map entry
- *   | `entry`                   | `(k,v)=>[k,v]`| Create a map entry
- *   | `keyComparator`           | `false`    | Comparator function (a,b) => (-1/0/1) that orders key values
- *   | `sorted`                  | `false`    | Assume array already sorted by key
- *   | `arrayElement`            | `false`    | Type of element used as a last resort to create new object instances
- *   | `collectionElementType`   | `false`    | Type of element used to create new elements in an array
- *   | `collectionElementFactory`| `false`    | Factory used to create new elements in an array 
+ *   | Option                     | Default       | Description
+ *   |----------------------------|---------------|-------------------------- 
+ *   | `elementFactory`           | `undefined`   | Factory used to create new object instances 
+ *   | `mergeInPlace`             | `false`       | Patch will copy properties into existing object rather than creating a new one
+ *   | `key`                      | `e=>e[0]`     | Function to extract key from a map entry
+ *   | `value`                    | `e=>e[1]`     | Function that extracts value from map entry
+ *   | `entry`                    | `(k,v)=>[k,v]`| Create a map entry
+ *   | `keyComparator`            | `undefined`   | Comparator function (a,b) => (-1/0/1) that orders key values
+ *   | `sorted`                   | `false`       | Assume array already sorted by key
+ *   | `arrayElement`             | `false`       | Type of element used as a last resort to create new object instances
+ *   | `collectionElementType`    | `undefined`   | Type of element used to create new elements in an array
+ *   | `collectionElementFactory` | `undefined`   | Factory used to create new elements in an array 
  *   | `collectionElementIdentity`| `(a,b)=>a===n`| Function to compare collection elements to see if they are the 'same'.
  *   | `identity`                 | `(a,b)=>a===n`| Function to compare objects to see if they are the 'same'.
  */
 const DEFAULT_OPTIONS = {
     elementFactory : undefined,
-    mergeInPlace : false,
+    mergeInPlace : false, 
     key: e=>e[0],
     value: e=>e[1],
     entry: (k,v)=>[k,v],
     map: false,
-    keyComparator: false,
+    keyComparator: undefined,
     sorted: false,
     elementType: false,
-    collectionElementType: false,
-    collectionElementFactory: false,
+    collectionElementType: undefined,
+    collectionElementFactory: undefined,
     collectionElementIdentity: (a,b)=>a===b,
     identity: (a,b)=>a===b
 }
@@ -61,7 +61,8 @@ class Options {
     getChildOptions(obj,name) {
         // TODO: We should perhaps look in this.defaults.elementType for a getAttrProps before we do anything else? 
         let options = this.defaults ? new Options(this.defaults) : Object.assign(new Options(this), { defaults: this });
-        delete options.elementType; /// element type is not inherited by child, but elementFactory is. TODO: document
+        delete options.elementType; 
+        delete options.elementTypeFactory;
         if (obj) {
             let child = obj[name];
             if (child && typeof child === 'object') {
